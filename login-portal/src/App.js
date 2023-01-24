8c8e2bbb2320edb9bc757db987c82a37ee493582
Steph Dunn <sdun8682@uni.sydney.edu.au>
2023-01-24 14:37:52 +1100

    Experimenting with exlcuding spaces in name input - commented out due to rendering bugs

--- login-portal/src/App.js
+++ login-portal/src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <section>
       <h1> EZiD Portal 🔑</h1>
-
+      {/* <img
+        className={classes.logo}
+        src="https://uploads-ssl.webflow.com/623a1fd881c44292399f3171/63c1595bc77fbce0341e4bff_logo.png"
+        alt="logo"
+      ></img> */}
       <SignUpForm />
     </section>
   );
--- login-portal/src/components/layout/SignUpForm.js
+++ login-portal/src/components/layout/SignUpForm.js
@@ -1,10 +1,62 @@
 import classes from "./SignUpForm.module.css";
 import Form from "../ui/Form";
+import React from "react";
 
 function SignUpForm(props) {
   // Handles behaviour to call API when form is submitted
   //   function submitHandler(event) {
   //     // Handle data inputs here
+  //   }
+
+  /**
+   * Holds the state of the input data for first and last name
+   */
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       inputFieldData: {
+  //         name: {
+  //           first: "",
+  //           last: "",
+  //           error: false,
+  //         },
+  //       },
+  //     };
+  //   }
+
+  /**
+   * Changes an input (first or last name) to have no spaces, as the user types it.
+   * @param {*} name - the value of the name parameter that has just changed
+   * @param {*} isFirst - a Boolean denoting if the name input being changed
+   * is the first name (true) or last name (false)
+   */
+  //   inputChange(name, isFirst) {
+  //     const nameInput = name.target.value;
+  //     if (isFirst) {
+  //       const lastInput = this.state.inputFieldData.last.val;
+  //       // Set first name to have no spaces
+  //       this.setState({
+  //         inputFieldData: {
+  //           name: {
+  //             first: nameInput.split(" ").join(""),
+  //             last: lastInput,
+  //             error: false,
+  //           },
+  //         },
+  //       });
+  //     } else {
+  //       // Set last name to have no spaces
+  //       const firstInput = this.state.inputFieldData.first.val;
+  //       this.setState({
+  //         inputFieldData: {
+  //           name: {
+  //             first: nameInput.split(" ").join(""),
+  //             last: firstInput,
+  //             error: false,
+  //           },
+  //         },
+  //       });
+  //     }
   //   }
 
   return (
@@ -13,11 +65,23 @@ function SignUpForm(props) {
       <form className={classes.form}>
         <div className={classes.formItem}>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" required id="firstName" placeholder="John" />
+          <input
+            type="text"
+            required
+            id="firstName"
+            placeholder="John"
+            maxLength="200"
+          />
         </div>
         <div className={classes.formItem}>
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" required id="lastName" placeholder="Appleseed" />
+          <input
+            type="text"
+            required
+            id="lastName"
+            placeholder="Appleseed"
+            maxLength="200"
+          />
         </div>
         <div className={classes.formItem}>
           <label htmlFor="eml">Email</label>
